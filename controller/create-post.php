35a7dc94d917f91fce3d03d0a4f10d928e94a68b
Isaac Child <isaacw.child@gmail.com>
2015-02-09 21:45:09 -0800

    6th commit error on create post fixed

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,6 @@
 <?php
- $title = filer_input(INPPUT_POST, "title", FILTER_SANITIZE_STRING);
+ $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
  $post = filter_input(INPUT_POST,  "post", FILTER_SANITIZE_STRING);
  
  echo "<p>Title: $title</p>";
- eho "<p>Post: $post</p>";
- 
+ echo "<p>Post: $post</p>";
