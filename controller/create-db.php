aa215b368b7151129682f75ef702504acee9664c
Isaac Child <isaacw.child@gmail.com>
2015-02-11 15:03:40 -0800

    11th commit had some missing work

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,36 +1,32 @@
 <?php
+
 require_once (__DIR__ . "/../model/config.php");
 
 $connection = new mysqli($host, $username, $password);
 
-if($connection->connect_error){
+if ($connection->connect_error) {
     die("<p>Error: " . $connection->connection_error . "</p>");
 }
 
 $exist = $connection->select_db($database);
 
-if(!$exist) {
+if (!$exist) {
     $query = $connection->query("CREATE DATABASE $database");
-    
-    if($query) {
-        echo "<p>Sucessfully created database: "  . $database . "</p>";
-    }
-}
 
-else {
+    if ($query) {
+        echo "<p>Sucessfully created database: " . $database . "</p>";
+    }
+} else {
     echo "<p>Database already exists.</p>";
 }
 $query = $connection->query("CREATE TABLE posts ("
-        ."id int(11)  NOT NULL  AUTO_INCREMENT,"
-        ."title varchar(225) NOT NULL,"
-        ."post text NOT NULL,"
+        . "id int(11)  NOT NULL  AUTO_INCREMENT,"
+        . "title varchar(225) NOT NULL,"
+        . "post text NOT NULL,"
         . "PRIMARY KEY (id))");
 
-if($query){
+if ($query) {
     echo "<p>Succesfully created table: post</p>";
-}
-else{
+} else {
     echo"<p>$connection->error</p>";
 }
-
- $connection->close();
--- controller/create-post.php
+++ controller/create-post.php
@@ -2,7 +2,7 @@
 
 require_once(__DIR__ . "/../model/config.php");
 
-$connection =  new mysqli($host, $username, $password, $database) ;
+$connection =  new mysqli($host, $username, $password) ;
 
  $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
  $post = filter_input(INPUT_POST,  "post", FILTER_SANITIZE_STRING);
