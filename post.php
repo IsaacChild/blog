927177b68ebe2b3187da7b26418ef1713632d16c
Isaac Child <isaacw.child@gmail.com>
2015-02-17 15:02:34 -0800

    13TH

--- post.php
+++ post.php
@@ -2,3 +2,4 @@
 require_once (__Dir__ ."/view/header.php");
 require_once (__DIR__ . "/view/form.php");
 require_once (__DIR__ . "/view/footer.php");
+?>
--- view/form.php
+++ view/form.php
@@ -1,7 +1,7 @@
 <?php
 require_once (__DIR__ . "/../model/config.php");
-
 ?>
+
 <h1>Create Blog Post</h1>
 
 <form method="post"  action="<?php echo $path . "controller/create-post.php"; ?>">
